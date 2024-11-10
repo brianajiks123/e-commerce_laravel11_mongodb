bdf0c882b67ed91213d4327039edd10cfc720df3
Brian Aji Pamungkas <brianajiks123@gmail.com>
2024-11-11 04:30:24 +0700

    add logic for admin login and logout

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -4,12 +4,31 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AdminController extends Controller
 {
     // Login
-    public function login() {
+    public function login(Request $request) {
+        if ($request->isMethod('post')) {
+            $data = $request->all();
+
+            // echo "<pre>"; print_r($data); die;
+            if (Auth::guard('admin')->attempt(['email'=>$data['email'], 'password'=>$data['password']])) {
+                return redirect('admin/dashboard');
+            }
+
+            return redirect()->back()->with('error_message', 'Invalid email or password');
+        }
+
         return view('admin.login');
+    }
+
+    // Logout
+    public function logout() {
+        Auth::guard('admin')->logout();
+
+        return redirect('admin/login');
     }
 
     // Dashboard
--- routes/web.php
+++ routes/web.php
@@ -9,8 +9,9 @@ Route::get('/', function () {
 
 Route::prefix('/admin')->namespace('App\Http\Controllers\Admin')->group(function() {
     Route::match(['get', 'post'], 'login', [AdminController::class, 'login']);
-    
+
     Route::group(['middleware'=>['admin']], function() {
         Route::match(['get', 'post'], 'dashboard', [AdminController::class, 'dashboard']);
+        Route::get('logout', [AdminController::class, 'logout']);
     });
 });
